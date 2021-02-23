1ba332f450088e161b1d8f5fd64c2893072c6965
anthonydominguez86 <adominguez11186@gmail.com>
2021-02-23 17:12:00 -0500

    constructors created employee and intern js written

---
+++ index.js
@@ -0,0 +1,9 @@
+const Employee = require ("./Employee")
+const Manager = require ("./Manager")
+const Intern = require ("./Intern")
+const Engineer  = require ("./Engineer")
+const inquirer = require("inquirer");
+
+const employee1 = new Employee ("Anthony", "15", "mail@mail.com");
+
+console.log (employee1)
