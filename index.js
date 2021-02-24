e1c42787a4c2ec23171a935628ceef6a1b2df0b7
anthonydominguez86 <adominguez11186@gmail.com>
2021-02-24 13:21:15 -0500

    constructor code written and working

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const Employee = require ("./Employee")
-const Manager = require ("./Manager")
-const Intern = require ("./Intern")
-const Engineer  = require ("./Engineer")
 const inquirer = require("inquirer");
+
+const Employee = require ("./lib/Employee");
+const Manager = require ("./lib/Manager");
+const Intern = require ("./lib/Intern");
+const Engineer  = require ("./lib/Engineer");
+
+const fs = require ("fs");
 
 const employee1 = new Employee ("Anthony", "15", "mail@mail.com");
 
